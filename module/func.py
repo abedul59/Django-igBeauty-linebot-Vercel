2055f5edb2c3b888b2c5d833cad074845e1464c6
pyfbsdk59 <pyfbsdk59@gmail.com>
2023-01-19 17:00:38 +0800

    fix pic

--- module/func.py
+++ module/func.py
@@ -16,4 +16,13 @@ def sendImageA(event):  #傳送圖片
     print(data['id'])
     print(data['Url'])
 
+    try:
+	        
+	        message = ImageSendMessage(
+	            original_content_url = data['Url'],
+	            preview_image_url = data['Url']
+	        )
+	        line_bot_api.reply_message(event.reply_token,message)
+	    except:
+	        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
 
