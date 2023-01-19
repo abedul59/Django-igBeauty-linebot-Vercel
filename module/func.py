1d63609653a555857df1d911441a806ef67bf277
pyfbsdk59 <pyfbsdk59@gmail.com>
2023-01-19 17:13:53 +0800

    fix pic3

--- module/func.py
+++ module/func.py
@@ -1,6 +1,8 @@
 from django.conf import settings
-
+from linebot import LineBotApi
+from linebot.models import TextSendMessage, ImageSendMessage, StickerSendMessage, LocationSendMessage, QuickReply, QuickReplyButton, MessageAction
 import os
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 
 def sendImageA(event):  #傳送圖片
     import  json, ssl, urllib.request
@@ -17,12 +19,11 @@ def sendImageA(event):  #傳送圖片
     print(data['Url'])
 
     try:
-	        
-	        message = ImageSendMessage(
+        message = ImageSendMessage(
 	            original_content_url = data['Url'],
 	            preview_image_url = data['Url']
 	        )
-	        line_bot_api.reply_message(event.reply_token,message)
-	except:
-	        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
 
